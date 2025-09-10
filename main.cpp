a59fdfcf88726830196cc15a289a89804f904b43
enrightw <enrightw@student.21-school.ru>
2025-09-10 13:59:10 +0300

    Внедрил функцию в main

--- main.cpp
+++ main.cpp
@@ -1,77 +1,88 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 struct Student {
-    std::string name;
-    int age;
-    std::string major;
-    double gpa;
+  std::string name;
+  int age;
+  std::string major;
+  double gpa;
 };
 
-void addStudent(std::vector<Student>& database) {
-    Student student;
-    std::cout << "Введите имя студента: ";
-    std::cin >> student.name;
-    std::cout << "Введите возраст студента: ";
-    std::cin >> student.age;
-    std::cout << "Введите специальность студента: ";
-    std::cin >> student.major;
-    std::cout << "Введите средний балл студента: ";
-    std::cin >> student.gpa;
+void addStudent(std::vector<Student> &database) {
+  Student student;
+  std::cout << "Введите имя студента: ";
+  std::cin >> student.name;
+  std::cout << "Введите возраст студента: ";
+  std::cin >> student.age;
+  std::cout << "Введите специальность студента: ";
+  std::cin >> student.major;
+  std::cout << "Введите средний балл студента: ";
+  std::cin >> student.gpa;
 
-    database.push_back(student);
-    std::cout << "Студент добавлен в базу данных.\n";
+  database.push_back(student);
+  std::cout << "Студент добавлен в базу данных.\n";
 }
 
-void print_sudent(const Student& student) {
-    std::cout << "Имя: " << student.name << "\n";
-    std::cout << "Возраст: " << student.age << "\n";
-    std::cout << "Специальность: " << student.major << "\n";
-    std::cout << "Средний балл: " << student.gpa << "\n\n";
+void print_sudent(const Student &student) {
+  std::cout << "Имя: " << student.name << "\n";
+  std::cout << "Возраст: " << student.age << "\n";
+  std::cout << "Специальность: " << student.major << "\n";
+  std::cout << "Средний балл: " << student.gpa << "\n\n";
 }
 
-void displayStudents(const std::vector<Student>& database) {
-    std::cout << "Список студентов:\n";
-    for (const Student& student : database) {
-        print_sudent(student);       
+void displayStudents(const std::vector<Student> &database) {
+  std::cout << "Список студентов:\n";
+  for (const Student &student : database) {
+    print_sudent(student);
+  }
+}
+
+void search_by_name_or_specialty(const std::vector<Student> &database) {
+  std::cout << "Введите имя студента или специальность: ";
+  std::string target;
+  std::cin >> target;
+  for (const Student &student : database) {
+    if (student.name == target || student.major == target) {
+      print_sudent(student);
+      return;
     }
-}
-
-void search_by_name_or_specialty(const std::vector<Student> &database, const std::string target) {
-    for(const Student &student : database) {
-        if(student.name == target || student.major == target) {
-            print_sudent(student);
-        }
-    }
+  }
+  std::cout << "Нет студента с таким именем или специальностью\n";
 }
 
 int main() {
-    std::vector<Student> database;
+  std::vector<Student> database;
 
-    int choice;
-    do {
-        std::cout << "Меню:\n";
-        std::cout << "1. Добавить студента\n";
-        std::cout << "2. Вывести список студентов\n";
-        std::cout << "0. Выход\n";
-        std::cout << "Выберите действие: ";
-        std::cin >> choice;
+  int choice;
 
-        switch (choice) {
-            case 1:
-                addStudent(database);
-                break;
-            case 2:
-                displayStudents(database);
-                break;
-            case 0:
-                std::cout << "Выход из программы.\n";
-                break;
-            default:
-                std::cout << "Неверный выбор. Попробуйте снова.\n";
-        }
-    } while (choice != 0);
+  do {
+    std::cout << "Меню:\n";
+    std::cout << "1. Добавить студента\n";
+    std::cout << "2. Вывести список студентов\n";
+    std::cout << "3. Поиск студентов по имени или специальности\n";
+    std::cout << "0. Выход\n";
+    std::cout << "Выберите действие: ";
+    std::cin >> choice;
 
-    return 0;
+    switch (choice) {
+    case 1:
+      addStudent(database);
+      break;
+    case 2:
+      displayStudents(database);
+      break;
+    case 3:
+
+      search_by_name_or_specialty(database);
+      break;
+    case 0:
+      std::cout << "Выход из программы.\n";
+      break;
+    default:
+      std::cout << "Неверный выбор. Попробуйте снова.\n";
+    }
+  } while (choice != 0);
+
+  return 0;
 }
